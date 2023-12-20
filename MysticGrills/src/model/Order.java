3159c77386b807fb7f94480ee45c8464c8684028
HalizaSyafa27 <111560506+HalizaSyafa27@users.noreply.github.com>
2023-12-20 18:29:03 +0700

    Update Order.java

--- MysticGrills/src/model/Order.java
+++ MysticGrills/src/model/Order.java
@@ -1,24 +1,26 @@
 package model;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
 	private int OrderID;
-//	private String OrderUser;
 	private String OrderName; 
 	private String OrderStatus;
 	private Date OrderDate;
 	private int OrderTotal;
 	private String OrderDesc; 
+	private List<OrderItem> orderItems;
 	
 	public Order(int orderID, String orderName, String orderStatus, Date orderDate, int orderTotal, String orderDesc) {
 		OrderID = orderID;
-//		OrderUser = orderUser;
 		OrderName = orderName;
 		OrderStatus = orderStatus;
 		OrderDate = orderDate;
 		OrderTotal = orderTotal;
 		OrderDesc = orderDesc;
+		this.orderItems = new ArrayList<>();
 	}
 
 	public int getOrderID() {
@@ -75,5 +77,24 @@ public class Order {
 		OrderDesc = orderDesc;
 	}
 	
+    // Metode untuk menambahkan OrderItem ke dalam pesanan
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+    }
+
+    // Metode untuk mengecek apakah pesanan sudah berisi menu tertentu
+    public boolean containsMenu(Menu menu) {
+        for (OrderItem orderItem : orderItems) {
+            if (orderItem.getItemName().equals(menu.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Metode untuk mendapatkan semua OrderItem dalam pesanan
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
 	
 }
