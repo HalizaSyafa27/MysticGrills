69fa590d64a21ae500309d6f703240ee617b2257
Callista Chow <98149486+kalisthea@users.noreply.github.com>
2023-12-20 11:45:11 +0700

    Update ReceiptController.java

--- MysticGrills/src/controller/ReceiptController.java
+++ MysticGrills/src/controller/ReceiptController.java
@@ -1,5 +1,27 @@
 package controller;
 
-public class ReceiptController {
+import java.util.ArrayList;
 
+import model.Receipt;
+import view.ReceiptView;
+
+public class ReceiptController {
+	
+	private ReceiptView receiptView;
+	
+	public void loadSearchedReceipt(String id) {
+		ArrayList<Receipt> searchedReceipt = Receipt.getReceipt(id);
+		receiptView.getTable().getItems().setAll(searchedReceipt);
+	}
+	
+	public void loadReceiptData() {
+		ArrayList<Receipt> receipts = Receipt.getAllReceipts();
+		receiptView.getTable().getItems().setAll(receipts);
+	}
+	
+	public ReceiptController(ReceiptView receiptView) {
+		this.receiptView = receiptView;
+		
+		loadReceiptData();
+	}
 }
