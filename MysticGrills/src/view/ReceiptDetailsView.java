986cb910c6988f2159a797f58cf74643cf3ff21c
Callista Chow <98149486+kalisthea@users.noreply.github.com>
2023-12-20 15:41:47 +0700

    Update ReceiptDetailsView.java

--- MysticGrills/src/view/ReceiptDetailsView.java
+++ MysticGrills/src/view/ReceiptDetailsView.java
@@ -1,21 +1,25 @@
 package view;
 
+import controller.ReceiptController;
 import javafx.beans.binding.Bindings;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Receipt;
-import model.ReceiptItem;
+import model.OrderItem;
 
 public class ReceiptDetailsView {
 
-    private TableView<ReceiptItem> table;
+    private TableView<OrderItem> table;
     private Stage stage;
+    private ReceiptController rc;
+    private ReceiptView receiptView;
 
     public ReceiptDetailsView(Stage stage) {
         this.stage = stage;
@@ -23,10 +27,15 @@ public class ReceiptDetailsView {
 
     // Method untuk menampilkan detail receipt
     public void showReceiptDetailsView(Receipt receipt) {
+    	
         VBox root = new VBox();
 
         table = createReceiptDetailsTable();
-        populateTable(receipt);
+        
+        table.getItems().clear();
+        
+        String id = receiptView.getIdInput().getText();
+        rc.loadReceiptDetail(id);
 
         GridPane form = createReceiptDetailsForm();
         VBox.setMargin(form, new Insets(20));
@@ -39,34 +48,34 @@ public class ReceiptDetailsView {
     }
 
     // Membuat tabel untuk menampilkan detail item pada receipt
-    private TableView<ReceiptItem> createReceiptDetailsTable() {
-        TableView<ReceiptItem> table = new TableView<>();
-        TableColumn<ReceiptItem, String> itemNameColumn = new TableColumn<>("Item Name");
+    private TableView<OrderItem> createReceiptDetailsTable() {
+        TableView<OrderItem> table = new TableView<>();
+        TableColumn<OrderItem, String> itemNameColumn = new TableColumn<>("Item Name");
         itemNameColumn.setCellValueFactory(cellData -> cellData.getValue().itemNameProperty());
 
-        TableColumn<ReceiptItem, Double> priceColumn = new TableColumn<>("Price");
+        TableColumn<OrderItem, Double> priceColumn = new TableColumn<>("Price");
         priceColumn.setCellValueFactory(cellData -> Bindings.createObjectBinding(() -> cellData.getValue().getPrice()));
 
-        TableColumn<ReceiptItem, Integer> quantityColumn = new TableColumn<>("Quantity");
+        TableColumn<OrderItem, Integer> quantityColumn = new TableColumn<>("Quantity");
         quantityColumn.setCellValueFactory(cellData -> cellData.getValue().quantityProperty().asObject());
 
         table.getColumns().addAll(itemNameColumn, priceColumn, quantityColumn);
         return table;
     }
 
-    // Mengisi tabel dengan data dari receipt
-    private void populateTable(Receipt receipt) {
-        // Bersihkan isi tabel sebelum menambahkan item baru
-        table.getItems().clear();
-
-        // Periksa apakah objek Receipt memiliki daftar item
-        if (receipt.getItems() != null) {
-            // Lakukan iterasi pada daftar item dan tambahkan ke tabel
-            for (ReceiptItem item : receipt.getItems()) {
-                table.getItems().add(item);
-            }
-        }
-    }
+//    // Mengisi tabel dengan data dari receipt
+//    private void populateTable(Receipt receipt) {
+//        // Bersihkan isi tabel sebelum menambahkan item baru
+//        table.getItems().clear();
+//
+////        // Periksa apakah objek Receipt memiliki daftar item
+////        if (receipt.getItems() != null) {
+////            // Lakukan iterasi pada daftar item dan tambahkan ke tabel
+////            for (OrderItem item : receipt.getItems()) {
+////                table.getItems().add(item);
+////            }
+////        }
+//    }
 
 
     // Membuat form (jika diperlukan)
@@ -75,4 +84,15 @@ public class ReceiptDetailsView {
         // Tambahkan komponen form sesuai kebutuhan
         return form;
     }
+
+	public TableView<OrderItem> getTable() {
+		return table;
+	}
+
+	public void setTable(TableView<OrderItem> table) {
+		this.table = table;
+	}
+    
+    
+
 }
